86070add48b271e690b1478923cbef7f06aaf101
cafeway <bcaranja@gmail.com>
2023-04-26 12:18:24 +0300

    ..

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static('public'));
+
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/assets/vendor/bootstrap/js/bootstrap.min.js', (req, res) => {
+  res.setHeader('Content-Type', mime.getType('js'));
+  res.sendFile(__dirname + '/public/assets/vendor/bootstrap/js/bootstrap.min.js');
 });
 
 app.listen(3000, function() {
